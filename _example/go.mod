d234749a9c57b782125f6daf9dfadcdb3d7b0906
Clint Egargo <67180945+egargo@users.noreply.github.com>
2025-11-14 00:21:00 +0800

    Update _example

--- _example/go.mod
+++ _example/go.mod
@@ -1,3 +1,5 @@
 module github.com/egargo/pigeon/_example
 
 go 1.25.2
+
+require github.com/egargo/pigeon v1.0.1
--- _example/main.go
+++ _example/main.go
@@ -6,14 +6,34 @@ import (
 	"github.com/egargo/pigeon"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("hello"))
+type Hello struct {
+	Name string
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	pigeon.JSON(w, http.StatusOK, pigeon.H{
+		"hello": "world",
+	})
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := pigeon.DecodeJSON(Hello{}, r.Body)
+	if err != nil {
+		pigeon.JSON(w, http.StatusBadRequest, pigeon.S{
+			Error: err.Error(),
+		})
+		return
+	}
+
+	pigeon.JSON(w, http.StatusOK, pigeon.H{
+		"hello": body.Name,
+	})
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", helloHandler)
+	mux.HandleFunc("GET /", helloWorldHandler)
+	mux.HandleFunc("POST /", greetHandler)
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":2122", mux)
 }
